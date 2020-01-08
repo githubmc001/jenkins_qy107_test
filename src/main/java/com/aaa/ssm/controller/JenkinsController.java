b47dca43163b6d692964015206adf2e8e039ba45
一捧 <1770166865@qq.com>
2020-01-08 16:43:17 +0800

    第一次推送

---
+++ src/main/java/com/aaa/ssm/controller/JenkinsController.java
@@ -0,0 +1,33 @@
+package com.aaa.ssm.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * fileName:TestJenkinsController
+ * description:
+ * author:zz
+ * createTime:2020/1/8 16:24
+ * version:1.0.0
+ */
+@RestController
+@RequestMapping("testJenkins")
+public class JenkinsController {
+
+    /**
+     * 打印信息
+     * @return
+     */
+    @RequestMapping("print")
+    public Object printInfo(){
+        Map map =new HashMap();
+        map.put("code",200);
+        map.put("message","success");
+        map.put("jenkins","CI SoftWare");
+        return map;
+    }
+}
+
